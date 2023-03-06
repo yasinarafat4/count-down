70cdc689800adccb4712f570190f300a424d0536
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-03-06 20:29:37 +0600

    count.js file added

--- count.js
+++ count.js
@@ -1,1 +1,17 @@
+function startCount() {
+  let count = 0;
+  intervalId = setInterval(() => {
+    document.getElementById("count-numbers").innerText = ++count;
+  }, 1000);
+}
 
+document.getElementById("start-btn").addEventListener("click", function () {
+  startCount();
+});
+document.getElementById("stop-btn").addEventListener("click", function () {
+  clearInterval(intervalId);
+});
+document.getElementById("reset-btn").addEventListener("click", function () {
+  clearInterval(intervalId);
+  document.getElementById("count-numbers").innerText = 0;
+});
